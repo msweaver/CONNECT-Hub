ef1676e60efefb3f66b9b09f8273e948056d68fa
Brian Humphrey <bhumphrey@5amsolutions.com>
2013-02-26 13:38:00 -0500

    wip CONN-150

--- interopgui/src/main/java/org/connectopensource/interopgui/services/EndpointService.java
+++ interopgui/src/main/java/org/connectopensource/interopgui/services/EndpointService.java
@@ -16,6 +16,5 @@ public interface EndpointService {
     public void saveEndpoint(Endpoint endpoint);
     public void saveEndpoints(List<Endpoint> endpoints);
     public List<Endpoint> getEndpoints(String homeCommunityId);
-    public List<Endpoint> getAllEndpoints();
-
+ 
 }
